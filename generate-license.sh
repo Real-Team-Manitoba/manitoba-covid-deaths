d6321726e4e9ce34f66335e7c68864ec6ad6e766
Buck Doyle <b@chromatin.ca>
2020-11-19 23:19:26 -0600

    Fix count extraction

--- generate-license.sh
+++ generate-license.sh
@@ -2,7 +2,7 @@ convert \
     blank-license.jpg \
     -font sticker.ttf \
     -pointsize 160 \
-    -draw "rotate 8 text 3345,1093 '$(jo -r .deaths deaths.json)'" \
+    -draw "rotate 8 text 3345,1093 '$(jq -r .deaths deaths.json)'" \
     -pointsize 35 \
     -fill white \
     -draw "rotate 8 text 3214,1155 'DEATHS AS OF $(jq -r .stickerDate deaths.json)'" \
