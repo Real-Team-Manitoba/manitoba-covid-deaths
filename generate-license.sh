cba86805bf801ae5f88b2a6b84f1e61dfeeae4bd
Buck Doyle <b@chromatin.ca>
2020-11-19 22:11:16 -0600

    Add sticker date prefix

--- generate-license.sh
+++ generate-license.sh
@@ -5,5 +5,5 @@ convert \
     -draw "rotate 8 text 3345,1093 '$(jo -r .deaths deaths.json)'" \
     -pointsize 35 \
     -fill white \
-    -draw "rotate 8 text 3214,1155 '$(jq -r .stickerDate deaths.json)'" \
+    -draw "rotate 8 text 3214,1155 'DEATHS AS OF $(jq -r .stickerDate deaths.json)'" \
     generated-license-$(jq -r .date deaths.json).jpg
