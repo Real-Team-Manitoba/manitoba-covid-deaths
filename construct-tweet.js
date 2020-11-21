a0e0a8a77f65552dd32342e76aa5bcb53ce468cf
Buck Doyle <b@chromatin.ca>
2020-11-21 13:50:08 -0600

    Add spaces between tweets for readability

--- construct-tweet.js
+++ construct-tweet.js
@@ -1,9 +1,9 @@
 import fs from 'fs';
 
 const deathsJson = JSON.parse(fs.readFileSync('deaths.json'));
-const [template, ...otherTemplates] = fs.readFileSync('tweet-templates.txt').toString().split("\n");
+const [template, ...otherTemplates] = fs.readFileSync('tweet-templates.txt').toString().split("\n").filter(l => l.length);
 
-fs.writeFileSync('tweet-templates.txt', [...otherTemplates, template].join('\n'));
+fs.writeFileSync('tweet-templates.txt', [...otherTemplates, template].join('\n\n') + '\n');
 
 const tweet = template.replace('DEATHS', deathsJson.deaths).replace('DATE', deathsJson.deathString);
 
