a7d492efae99a82382c186c9c00b240ea6aa3678
Buck Doyle <b@chromatin.ca>
2020-11-21 09:22:26 -0600

    Change tweets to happen in order
    It seems a bit strange to be needing to commit this but it
    also seems like the easiest way to not have repeated tweets
    by preserving state between runs.

--- construct-tweet.js
+++ construct-tweet.js
@@ -1,9 +1,9 @@
 import fs from 'fs';
 
 const deathsJson = JSON.parse(fs.readFileSync('deaths.json'));
-const templates = fs.readFileSync('tweet-templates.txt').toString().split("\n");
+const [template, ...otherTemplates] = fs.readFileSync('tweet-templates.txt').toString().split("\n");
 
-const template = templates[Math.floor(Math.random() * templates.length)];
+fs.writeFileSync('tweet-templates.txt', [...otherTemplates, template].join('\n'));
 
 const tweet = template.replace('DEATHS', deathsJson.deaths).replace('DATE', deathsJson.deathString);
 
